3f559ab31c4b1ed5ed906a0e403fa98ac3dfe69d
esteban dardilac <esteban.dardillac@gmail.com>
2023-03-16 16:30:49 +0100

    grande avancé  projet

--- Assets/tpose.cs
+++ Assets/tpose.cs
@@ -1,13 +1,24 @@
-public NavMeshAgent agent;
-public Transform player;
+using UnityEngine;
 
-void Start()
+public class tpose : MonoBehaviour
 {
-    agent = GetComponent<NavMeshAgent>();
-    player = GameObject.FindGameObjectWithTag("Player").transform;
-}
+    public UnityEngine.AI.NavMeshAgent agent;
+    public Transform player;
+    public bool isRunning = false;
 
-void Update()
-{
-    agent.SetDestination(player.position);
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        agent.SetDestination(player.position);
+
+         // Active l'animation de course si l'ennemi est en train de courir
+        if (isRunning && agent.velocity.magnitude > 0.1f) {
+            GetComponent<Animator>().SetTrigger("Run");
+        }
+    }
 }
