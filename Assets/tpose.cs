c10e87b796f6f535454753c255b6bd47fe954c94
esteban dardilac <esteban.dardillac@gmail.com>
2023-03-16 10:50:09 +0100

    tpose

--- Assets/tpose.cs
+++ Assets/tpose.cs
@@ -1,36 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+public NavMeshAgent agent;
+public Transform player;
 
-public class tpose : MonoBehaviour
+void Start()
 {
-    public Transform target;
-    public Transform player1;
-    public Transform player2;
-    public float moveSpeed = 5f;
-    public float rotationSpeed = 5f;
+    agent = GetComponent<NavMeshAgent>();
+    player = GameObject.FindGameObjectWithTag("Player").transform;
+}
 
-    void Update()
-    {
-        // Calculer la direction du player2 vers le player1
-        Vector3 direction = target.position - player2.position;
-        direction.y = 0;
-
-        // Faire tourner le player2 vers le player1
-        Quaternion rotation = Quaternion.LookRotation(direction);
-        player2.rotation = Quaternion.Slerp(player2.rotation, rotation, rotationSpeed * Time.deltaTime);
-
-        // Faire avancer le player2 vers le player1
-        player2.Translate(0, 0, moveSpeed * Time.deltaTime);
-
-        // Vérifier si le player2 a rattrapé le player1
-        if (Vector3.Distance(player1.position, player2.position) <= 1f)
-        {
-            Debug.Log("Player 2 a attrapé Player 1 !");
-        }
-
-        // Faire tourner le player1
-        float horizontalInput = Input.GetAxis("Horizontal");
-        player1.Rotate(0, horizontalInput * rotationSpeed * Time.deltaTime, 0);
-    }
+void Update()
+{
+    agent.SetDestination(player.position);
 }
